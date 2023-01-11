8f07fcacaa3d94d802ebeef30922af8b03043c2d
Cody Smith <codeman1926@gmail.com>
2023-01-11 11:52:53 -0500

    add venv and .env file

--- todo_list_django/settings.py
+++ todo_list_django/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k)fz4m*3-!q(bg)iv*g1lq0g7t@^92b#1z9v*e6_d6c-myq(fe'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -51,6 +51,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'todo_list_django.urls'
+
+# Secret key is stored in .env file
+SECRET_KEY = config("SECRET_KEY")
 
 TEMPLATES = [
     {
